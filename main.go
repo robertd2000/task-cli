0877a7cd8ddff9ec98aa54e36579b77ae821147d
robertd2000 <zdabaghov@mail.ru>
2025-03-14 17:43:59 +0300

    CreateTask

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/robertd2000/task-cli/internals/models"
 	"github.com/robertd2000/task-cli/internals/service"
 )
 
@@ -14,57 +12,17 @@ func main() {
 
 	fmt.Print(args)
 
-	task := &models.Task{
-		Id:          1,
-		Description: args[0],
-		Status:      "todo",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	service.CreateTask("test")
+
+	tasks:= service.GetTasks()
+
+	for _, task := range tasks {
+		fmt.Printf("%+v\n", task)
 	}
 
-	tasks := []models.Task{*task}
-	
-	s, err := service.SerializeToJSON(tasks)
+	service.CreateTask("test 2")
 
-	if err != nil {
-		fmt.Printf("unable to serialize task: %w", err)
+	for _, task := range tasks {
+		fmt.Printf("%+v\n", task)
 	}
-
-	err = service.SaveToJSON("db.json", s)
-
-	if err != nil {
-		fmt.Printf("unable to save to json: %w", err)
-	}
-
-	stream, err := service.ReadFromJSON("db.json")
-
-	if err != nil {
-		fmt.Printf("unable to read from json: %w", err)
-	}
-
-	t, err := service.DeserializeFromJSON[[]models.Task](stream)
-
-	if err != nil {
-		fmt.Printf("unable to deserialize task: %w", err)
-	}
-
-	fmt.Println(t)
-
-	t2 := &models.Task{
-		Id:          2,
-		Description: "ccc",
-		Status:      "todo",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	tasks = append(tasks, *t2)
-
-	s, err = service.SerializeToJSON(tasks)
-
-	if err != nil {
-		fmt.Printf("unable to serialize task: %w", err)
-	}
-
-	service.SaveToJSON("db.json", s)
 }
