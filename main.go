0aa4733f3abd2862a8608d8a3bc38a08b2437a36
robertd2000 <zdabaghov@mail.ru>
2025-03-14 16:45:42 +0300

    db

--- main.go
+++ main.go
@@ -3,10 +3,34 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/robertd2000/task-cli/internals/models"
+	"github.com/robertd2000/task-cli/internals/service"
 )
 
 func main() {
 	args := os.Args[1:]
 
 	fmt.Print(args)
+
+	task := &models.Task{
+		Id:          1,
+		Description: args[0],
+		Status:      "todo",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+	
+	s, err := service.SerializeTask(task)
+
+	if err != nil {
+		fmt.Printf("unable to serialize task: %w", err)
+	}
+
+	err = service.SaveToJSON(s)
+
+	if err != nil {
+		fmt.Printf("unable to save to json: %w", err)
+	}
 }
