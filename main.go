0d3a31858e955cfe994e881ed2c1621107c02646
robertd2000 <zdabaghov@mail.ru>
2025-03-15 20:55:48 +0300

    operations

--- main.go
+++ main.go
@@ -10,7 +10,28 @@ import (
 
 func main() {
 	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("No arguments provided")
+		return
+	}
 
+	switch operation := args[0]; operation {
+	case "add":
+		fmt.Println("Create task")
+	case "update":
+		fmt.Println("Update task")
+	case "delete":
+		fmt.Println("Delete task")
+	case "list":
+		fmt.Println("Get task")
+	case "mark-in-progress":
+		fmt.Println("Get task")
+	case "mark-done":
+		fmt.Println("Get task")
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
 	fmt.Print(args)
 		taskRepository := repository.NewTaskRepository("db.json")
 		taskService := service.NewTaskService(taskRepository)
