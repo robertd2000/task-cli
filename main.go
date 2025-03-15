e3e58b35bfa8be121608f91226227d97cb43943d
robertd2000 <zdabaghov@mail.ru>
2025-03-15 20:58:18 +0300

    add

--- main.go
+++ main.go
@@ -15,9 +15,19 @@ func main() {
 		return
 	}
 
+	taskRepository := repository.NewTaskRepository("db.json")
+	taskService := service.NewTaskService(taskRepository)
+
 	switch operation := args[0]; operation {
 	case "add":
-		fmt.Println("Create task")
+		if len(args) < 2 {
+			fmt.Println("No description provided")
+			return
+		}
+
+		description := args[1]
+
+		taskService.CreateTask(description)
 	case "update":
 		fmt.Println("Update task")
 	case "delete":
@@ -32,9 +42,8 @@ func main() {
 		fmt.Println("Invalid operation")
 		return
 	}
-	fmt.Print(args)
-		taskRepository := repository.NewTaskRepository("db.json")
-		taskService := service.NewTaskService(taskRepository)
+
+
 	// taskService.CreateTask("test 7")
 	taskService.UpdateTask(0, "test 0")
 }
