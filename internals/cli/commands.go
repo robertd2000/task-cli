5fd633a612bc51fac818ba1c209ca76fd68c30c6
robertd2000 <zdabaghov@mail.ru>
2025-03-16 11:15:23 +0300

    Add

--- internals/cli/commands.go
+++ internals/cli/commands.go
@@ -1,1 +1,24 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/robertd2000/task-cli/internals/service"
+)
+
+type Commands struct{
+	taskService service.TaskService
+}
+
+func (c *Commands) Add(args []string) {
+	if len(args) < 2 {
+		fmt.Println("No description provided")
+		return
+	}
+
+	description := args[1]
+
+	c.taskService.CreateTask(description)
+
+	fmt.Println("Task created")
+}
