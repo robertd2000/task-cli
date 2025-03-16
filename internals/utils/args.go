7175b0345d7a6c45e0a1acf33adfd8818d874286
robertd2000 <zdabaghov@mail.ru>
2025-03-16 12:02:07 +0300

    GetId

--- internals/utils/args.go
+++ internals/utils/args.go
@@ -1,9 +1,27 @@
 package utils
 
+import (
+	"log"
+	"strconv"
+)
+
 func GetDescription(args []string, idx int) string {
 	if len(args) < idx+1 {
 		return ""
 	}
 
 	return args[idx]
+}
+
+func GetId(args []string, idx int) int {
+	if len(args) < idx+1 {
+		return 0
+	}
+
+	id, err := strconv.Atoi(args[idx])
+	if err != nil {
+		log.Fatal("invalid id: %w", err)
+	}
+
+	return id
 }
